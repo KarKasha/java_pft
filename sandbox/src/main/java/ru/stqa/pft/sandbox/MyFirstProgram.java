8c13b3658e27e735607a77964d3e2df3df2f65c2
KarKasha <65483883+KarKasha@users.noreply.github.com>
2020-05-22 22:15:56 +0300

    Update MyFirstProgram.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -15,12 +15,14 @@ public class MyFirstProgram {
 
   }
     public static void hello(String somebody) {
-           System.out.println("Hello, "+ somebody + "!");
+
+    System.out.println("Hello, "+ somebody + "!");
     }
     public static double area (double len){
     return len*len;
     }
     public static double area (double a, double b){
+
     return a*b;
     }
 }
